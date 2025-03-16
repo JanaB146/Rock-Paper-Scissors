a1e03d4319139db16f98d549bbe3f336c4141e0b
JanaB146 <jana.bogner@gmx.de>
2025-03-16 20:16:04 +0100

    change

--- rock.js
+++ rock.js
@@ -6,20 +6,32 @@ function startGame() {
 
     choices = ["rock", "paper", "scissors"];
     computers_choice = Math.floor(Math.random() * 3);
-    computers_choice = choices(computers_choice);    
-}
-function checkInput() {
-    if (computers_choice == null) {
-        return;
-    }
-    while (tries > 0) {
-        players_choice = document.getElementById("choice").value;
-        if (computers_choice == players_choice) {
-            document.getElementById("result").innerText = "Draw";
-        } else {
-            document.getElementById("result").innerText = "Not a Draw."
+    computers_choice = choices(computers_choice);
+    
+    players_choice = document.getElementById("choice").value;
+
+
+    function checkInput() {
+        if (computers_choice == null) {
+            return;
+        }
+        while (tries > 0) {
+            if (players_choice == "#rock") {
+                document.getElementById("result").innerText = "./pics/Rock_Bubble.png";
+            } else if (players_choice =="#paper") {
+                document.getElementById("result").innerText = "./pics/Paper_Bubble.png";
+            } else {
+                document.getElementById("result").innerText = "./pics/Scissor_Bubble.png";
+            }
+            
+            if (computers_choice == players_choice) {
+                document.getElementById("start").innerText = "Draw";
+            } else {
+                document.getElementById("start").innerText = "Not a Draw."
+                tries = tries - 1;
+            }
+            return false;
         }
     }
-
 
 }
